ab0b30415d4b1f1b32f700db53bc16dcc466cbbd
Arthur POIRET <dropsnorz@gmail.com>
2016-08-31 23:27:41 +0200

    initial commit

---
+++ CyrodiilAction.lua
@@ -0,0 +1,38 @@
+
+
+CyrodiilAction = {} 
+CyrodiilAction.name = "CyrodiilAction"
+
+ 
+function CyrodiilAction:Initialize()
+
+	EVENT_MANAGER:RegisterForEvent("event_keep", EVENT_KEEP_UNDER_ATTACK_CHANGED, self.OnKeepStatusUpdate)
+  CHAT_SYSTEM:AddMessage("CyrodiilAction loaded !")
+
+  -- ...but we don't have anything to initialize yet. We'll come back to this.
+end
+
+function CyrodiilAction.OnAddOnLoaded(event, addonName)
+  -- The event fires each time *any* addon loads - but we only care about when our own addon loads.
+  if addonName == CyrodiilAction.name then
+    CyrodiilAction:Initialize()
+  end
+end
+
+function CyrodiilAction.OnKeepStatusUpdate(_, keepID, battlegroundContext, underAttack)
+    
+    --Optionally hide IC district battles
+    if GetKeepType(keepID) == KEEPTYPE_IMPERIAL_CITY_DISTRICT then
+            return;
+    end
+
+    if underAttack then
+    	d("Keep under attack.")
+    else
+      d("Keep safe.")
+
+    end
+end
+
+
+EVENT_MANAGER:RegisterForEvent(CyrodiilAction.name, EVENT_ADD_ON_LOADED, CyrodiilAction.OnAddOnLoaded)
